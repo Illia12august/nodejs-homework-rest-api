eb9dd5646407d021b4cb5ac002fb0f03580533ce
Illia12august <121337310+Illia12august@users.noreply.github.com>
2023-11-13 22:56:29 +0300

    update

--- server.js
+++ server.js
@@ -1,15 +1,5 @@
 const app = require("./app");
-const mongoose = require("mongoose");
-mongoose.set("strictQuery", true);
-mongoose
-  .connect(
-    "mongodb+srv://illia:iLZGpVV5qruM4HxE@cluster0.suukots.mongodb.net/?retryWrites=true&w=majority"
-  )
-  .then(
-    app.listen(3000, () => {
-      console.log("Server running. Use our API on port: 3000");
-    })
-  )
-  .catch(() => {
-    process.exit(1);
-  });
+
+app.listen(3000, () => {
+  console.log("Server running. Use our API on port: 3000");
+});
